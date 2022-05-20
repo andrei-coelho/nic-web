a75253caba61337aa89f97b3eaa1d0fb66b97cfc
unknown <andreifcoelho@gmail.com>
2022-05-19 21:03:58 -0300

    adicionado area de configurações do dropbox

--- api/services/admin/config.php
+++ api/services/admin/config.php
@@ -1,7 +1,6 @@
 <?php 
 
-function list_dropbox_tokens($dropbox_tk_id = 0){
-
+function list_dropbox_tokens(int $dropbox_tk_id = 0){
 
     $where = $dropbox_tk_id > 0 ? " WHERE dropbox_tk.id = $dropbox_tk_id;" : ";";
     
@@ -21,7 +20,32 @@ function list_dropbox_tokens($dropbox_tk_id = 0){
     $total = $query->rowCount();
     if($total == 0) _error();
 
-    return _response($total > 1 ? $query->fetchAllAssoc() : $query->fetchAssoc());
+    return _response($dropbox_tk_id == 0 ? $query->fetchAllAssoc() : $query->fetchAssoc());
         
 }
 
+function salvar_dropbox_token($email, $senha, $key, $secret, $token, int $espaco){
+    
+    if(!_exec("INSERT INTO 
+        dropbox_tk (refresh_token, account_email, account_senha, app_key, secret_key, limit_size)
+        VALUES     ('$token', '$email', '$senha', '$key', '$secret', $espaco)
+    ")) _error(500, 'server error');
+
+}
+
+function excluir_dropbox_token(int $dropbox_tk_id){
+
+    try {
+        _query("DELETE FROM dropbox_tk WHERE id = $dropbox_tk_id");
+    } catch (\Exception $th) {
+        _error(500, 'Não é possível excluir a conta Dropbox porque tem clientes usando ela.');
+    }
+
+}
+
+function editar_espaco(int $dropbox_tk_id, int $espaco){
+
+    if(!_exec("UPDATE dropbox_tk SET limit_size = $espaco WHERE id = $dropbox_tk_id"))
+        _error();
+    return _response([], "Dados atualizados com sucesso!");
+}
